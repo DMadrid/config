b7011582120989897842de196e807514e4b43a59
Madrid <dem283@cornell.edu>
2020-12-21 13:05:23 -0500

    copy zshrc

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,3 +1,3 @@
 chsh -s /bin/zsh &&
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+cp ../dotfiles/.zshrc ~/.zshrc
